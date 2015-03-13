d37d506d20cef730cc5a4b811edc29615b575518
Fiete Winter <fiete.winter@uni-rostock.de>
2015-03-13 18:13:04 +0100

    master: add wrapping function

---
+++ matlab/wrappedAD.m
@@ -0,0 +1,29 @@
+function d = wrappedAD(x, X)
+% compute absolute distance from zero taking interval wrapping into account
+
+%*****************************************************************************
+% Copyright (c) 2015      Fiete Winter                                       *
+%                         Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock, Germany  *
+%                                                                            *
+% This file is part of the supplementary material for Fiete Winter's         *
+% scientific work and publications                                           *
+%                                                                            *
+% You can redistribute the material and/or modify it  under the terms of the *
+% GNU  General  Public  License as published by the Free Software Foundation *
+% , either version 3 of the License,  or (at your option) any later version. *
+%                                                                            *
+% This Material is distributed in the hope that it will be useful, but       *
+% WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+% or FITNESS FOR A PARTICULAR PURPOSE.                                       *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy of the GNU General Public License along   *
+% with this program. If not, see <http://www.gnu.org/licenses/>.             *
+%                                                                            *
+% http://github.com/fietew/publications           fiete.winter@uni-rostock.de*
+%*****************************************************************************
+
+d = wrappedDistance(x, X, @(x) abs(x));
+end
