06180368c6a06d65941a39a473a9e3d60507ff0c
Fiete Winter <fiete.winter@uni-rostock.de>
2015-08-12 22:36:14 +0200

    master: optional boolean to activate parallel evaluation

--- matlab/exhaustive_evaluation.m
+++ matlab/exhaustive_evaluation.m
@@ -1,4 +1,4 @@
-function exhaustive_evaluation(func, param_names, param_values, params)
+function exhaustive_evaluation(func, param_names, param_values, params, parallel)
 %
 % inputs:
 %   func         - function expecting 'params' as the only input argument, 
@@ -6,6 +6,8 @@ function exhaustive_evaluation(func, param_names, param_values, params)
 %   param_names  - name of parameters which shall be iterated over, cell [1xN]
 %   param_values - different value tuples for the parameters, cell [MxN]
 %   params       - scalar structure containing all parameters
+%   parallel     - if the distributed computing toolbox is available
+%                  different tuples are processed in parallel (default: true)
 %
 % example:
 %   f = @(x) display(x);
@@ -19,8 +21,12 @@ function exhaustive_evaluation(func, param_names, param_values, params)
 %
 %   exhaustive_evaluation(func, param_names, param_values, params)
 
+if ~exist( 'parallel', 'var' )
+  parallel = true;
+end
+
 % check for parallel computing toolbox
-if license('test', 'distrib_computing_toolbox')
+if license('test', 'distrib_computing_toolbox') && parallel
   parfor idx=1:size(param_values,1)
     tmp = assignfield_recursive(params, param_names, param_values(idx,:));
     func(tmp);
