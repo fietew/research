5665c1d25a83b3cb8c87981de68a8b72846cd2c4
Fiete Winter <fiete.winter@uni-rostock.de>
2018-06-06 10:08:58 +0200

    reorder months in publications

--- doc/conf.py
+++ doc/conf.py
@@ -90,18 +90,18 @@ class CustomSortingStyle(SortingStyle):
     
     
     month_indices = {
-        'January'   : '01',
-        'February'  : '02', 
-        'March'     : '03',
-        'April'     : '04',
-        'May'       : '05',
-        'June'      : '06',
-        'July'      : '07',
-        'August'    : '08',
-        'September' : '09',
-        'October'   : '10',
-        'November'  : '11',
-        'December'  : '12'
+        'January'   : '12',
+        'February'  : '11', 
+        'March'     : '10',
+        'April'     : '09',
+        'May'       : '08',
+        'June'      : '07',
+        'July'      : '06',
+        'August'    : '05',
+        'September' : '04',
+        'October'   : '03',
+        'November'  : '02',
+        'December'  : '01'
     }
     
     
