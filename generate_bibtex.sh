e0a511f5e730b6ff4549c82b41d219028492ce4e
Fiete Winter <fiete.winter@uni-rostock.de>
2018-12-04 15:15:36 +0100

    fix search pattern for bibtool

--- generate_bibtex.sh
+++ generate_bibtex.sh
@@ -3,5 +3,5 @@
 grep "@.*{.*," publications.bib | grep -o -P "(?<={).*(?=,)" | while read -r line ; do
     echo "Processing $line"
     # https://www.ctan.org/pkg/bibtool
-    ./bibtool -r bibtex.rsc -X $line publications.bib -o doc/bibtex/$line.txt
+    ./bibtool -r bibtex.rsc -X $line$ publications.bib -o doc/bibtex/$line.txt
 done
