cc4351b8cfeb2b4c9371ee47ee222ef738e36111
Fiete Winter <fiete.winter@uni-rostock.de>
2019-04-04 10:36:15 +0200

    remove obsolete shell scripts as they are moved to the tools repo (subtree)

--- generate_bibtex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-grep "@.*{.*," publications.bib | grep -o -P "(?<={).*(?=,)" | while read -r line ; do
-    echo "Processing $line"
-    # https://www.ctan.org/pkg/bibtool
-    ./bibtool -r bibtex.rsc -X $line$ publications.bib -o doc/bibtex/$line.txt
-done
