ffd897da1619df913f5a1f256a4d909bf356461e
Fiete Winter <fiete.winter@uni-rostock.de>
2019-04-04 14:55:14 +0200

    add DAGA 2019 talk to publications

---
+++ tools/bash/bibtex.rsc
@@ -0,0 +1,6 @@
+print.line.length = { 999 }
+print.align.key = { 0 }
+print.use.tab = {off}
+pass.comments= {off}
+preserve.key.case = {on}
+delete.field = { repo }
--- tools/bash/generate_bibtex.sh
+++ tools/bash/generate_bibtex.sh
@@ -9,7 +9,7 @@ fi
 bibfile=$1
 directory=$2
 
-grep "@.*{.*," publications.bib | grep -o -P "(?<={).*(?=,)" | while read -r line ; do
+grep "@.*{.*," $bibfile | grep -o -P "(?<={).*(?=,)" | while read -r line ; do
     echo "Processing $line"
     # https://www.ctan.org/pkg/bibtool
     bibtool -r bibtex.rsc -X $line$ $bibfile -o $directory/$line.txt
